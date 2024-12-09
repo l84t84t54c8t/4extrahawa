f6bf371308030bfe761a787b64412194afeb3767
l888lmn <deakoking85@gmail.com>
2024-12-09 23:33:19 +0300

    Update Dev.py

--- plugins/Dev.py
+++ plugins/Dev.py
@@ -2,7 +2,7 @@ import os
 from asyncio import gather
 
 from AlinaMusic import app
-from config import USER_OWNER
+from config import USER_OWNER, OWNER_ID
 from pyrogram import Client, filters
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
@@ -13,21 +13,21 @@ from strings.filter import command
 async def huhh(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://graph.org/file/b4ace5c5aec2901efed59.jpg",
-        caption=f"""**[⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝘼𝙇𝙄𝙉𝘼 - 🧑🏻‍💻🖤 گەشەپێدەران](t.me/MGIMT)**\n••┉┉┉┉┉••🝢••┉┉┉┉┉••\n**بەخێربێی ئەزیزم{message.from_user.mention} بۆ بەشی گەشەپێدەرانی بۆت🕷️•**\n**بۆ هەبوونی هەرکێشە و پرسیارێك پەیوەندی بە گەشەپێدەر بکە لەڕێگای دووگمەکانی خوارەوە♥•**""",
+        caption=f"""**[⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝙃𝙖𝙬𝙖𝙡 - 🧑🏻‍💻🖤 گەشەپێدەران](t.me/Haawall)**\n••┉┉┉┉┉••🝢••┉┉┉┉┉••\n**بەخێربێی ئەزیزم{message.from_user.mention} بۆ بەشی گەشەپێدەرانی بۆت🕷️•**\n**بۆ هەبوونی هەرکێشە و پرسیارێك پەیوەندی بە گەشەپێدەر بکە لەڕێگای دووگمەکانی خوارەوە♥•**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("᳒ᯓ محمد ✬", url=f"https://t.me/IQ7amo"),
+                    InlineKeyboardButton("H𝐀𝐖𝐀𝐋 ʲ ↱ˡⁱᶠᵉ↰🐍", user_id=OWNER_ID"),
                 ],
                 [
-                    InlineKeyboardButton("𐇮 ﮼ﺣ‌ّــەمــە 🇧🇷 𐇮", url=f"https://t.me/VTVIT"),
+                    InlineKeyboardButton("𐇮 ﮼ﺣ‌ّــەمــە 🇧🇷 𐇮", url=f"https://t.me/IQ7amo"),
                 ],
                 [
-                    InlineKeyboardButton("⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝘼𝙇𝙄𝙉𝘼", url=f"https://t.me/MGIMT"),
+                    InlineKeyboardButton("⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝙃𝙖𝙬𝙖𝙡", url=f"https://t.me/Haawall"),
                 ],
                 [
                     InlineKeyboardButton(
-                        "『𓏺کەناڵی سەرەکی』", url=f"https://t.me/EHS4SS"
+                        "『گرووپ بۆت』", url=f"https://t.me/piec0flife"
                     ),
                 ],
             ]
@@ -39,23 +39,23 @@ async def huhh(client: Client, message: Message):
 async def iqbot(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://graph.org/file/426283f861812c31153d1.jpg",
-        caption=f"""**• باشترین بۆتی گۆرانی بۆ کورد**\n\n**• پاراستن و داگرتن و وەڵامدانەوە\n\n**• 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋 -› [𝑴𝒖𝒉𝒂𝒎𝒎𝒆𝒅](t.me/IQ7amo)**\n**• 𝖢𝗁𝖺𝗇𝗇𝖾𝗅 -› [𝑺𝒐𝒖𝒓𝒄𝒆 𝑨𝒍𝒊𝒏𝒂](t.me/MGIMT)**""",
+        caption=f"""**• باشترین بۆتی گۆرانی بۆ کورد**\n\n**• پاراستن و داگرتن و وەڵامدانەوە\n\n**• 𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋 -› [𝑯𝒂𝒘𝒂𝒍](t.me/Hawaallll)**\n**• 𝖢𝗁𝖺𝗇𝗇𝖾𝗅 -› [𝑺𝒐𝒖𝒓𝒄𝒆 𝑯𝒂𝒘𝒂𝒍](t.me/Haawall)**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("◌sᴏᴜʀᴄᴇ ᴀʟɪɴᴀ◌", url=f"https://t.me/MGIMT"),
+                    InlineKeyboardButton("◌sᴏᴜʀᴄᴇ ʜᴀᴡᴀʟ◌", url=f"https://t.me/Haawall"),
                 ],
                 [
                     InlineKeyboardButton(
                         "• زیادم بکە بۆ گرووپت 🎻",
-                        url=f"https://t.me/IQMCBOT?startgroup=true",
+                        url=f"https://t.me/{app.username}?startgroup=true",
                     ),
                 ],
             ]
         ),
     )
 
-
+"""
 @app.on_message(
     filters.command(["ڕێکخەری بۆت", "/bot", "بۆتی گۆرانی", "ڕێکخەر", "/maker"], "")
 )
@@ -87,8 +87,8 @@ async def huhh(client: Client, message: Message):
         ),
     )
 
-
-@app.on_message(command(["@VTVIT"]))
+"""
+@app.on_message(command(["@IQ7amo"]))
 async def yas(client, message):
 
     usr = await client.get_chat("VTVIT")
@@ -96,7 +96,7 @@ async def yas(client, message):
     photo = await app.download_media(usr.photo.big_file_id)
     await message.reply_photo(
         photo,
-        caption=f"**[⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝘼𝙇𝙄𝙉𝘼 - سەرچاوەی زیرەك 🧑🏻‍💻](t.me/MGIMT)\nزانیاری گەشەپێدەری دووەمی بۆت\n↜︙𝐍𝐀𝐌𝐄 ↬:{name}\n↜︙𝐔𝐒𝐄𝐑 ↬ :@{usr.username}\n↜︙𝐈𝐃 ↬ :`{usr.id}`\n↜︙𝐁𝐈𝐎 ↬: {usr.bio}**",
+        caption=f"**[⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝙃𝙖𝙬𝙖𝙡 - سەرچاوەی هەواڵ 🧑🏻‍💻](t.me/Haawall)\nزانیاری گەشەپێدەری بۆت\n↜︙𝐍𝐀𝐌𝐄 ↬:{name}\n↜︙𝐔𝐒𝐄𝐑 ↬ :@{usr.username}\n↜︙𝐈𝐃 ↬ :`{usr.id}`\n↜︙𝐁𝐈𝐎 ↬: {usr.bio}**",
         reply_markup=InlineKeyboardMarkup(
             [
                 [InlineKeyboardButton(name, url=f"https://t.me/{usr.username}")],
@@ -112,7 +112,7 @@ async def yas(client, message):
     photo = await app.download_media(usr.photo.big_file_id)
     await message.reply_photo(
         photo,
-        caption=f"**[⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝙄𝙌 - 🧑🏻‍💻🖤 پڕۆگرامساز](t.me/MGIMT)\nزانیاری پڕۆگرامسازی سەرچاوە\n↜︙𝐍𝐀𝐌𝐄 ↬:{name}\n↜︙𝐔𝐒𝐄𝐑 ↬ :@{usr.username}\n↜︙𝐈𝐃 ↬ :`{usr.id}`\n↜︙𝐁𝐈𝐎 ↬: {usr.bio}**",
+        caption=f"**[⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝙃𝙖𝙬𝙖𝙡 - 🧑🏻‍💻🖤 پڕۆگرامساز](t.me/Haawall)\nزانیاری پڕۆگرامسازی سەرچاوە\n↜︙𝐍𝐀𝐌𝐄 ↬:{name}\n↜︙𝐔𝐒𝐄𝐑 ↬ :@{usr.username}\n↜︙𝐈𝐃 ↬ :`{usr.id}`\n↜︙𝐁𝐈𝐎 ↬: {usr.bio}**",
         reply_markup=InlineKeyboardMarkup(
             [
                 [InlineKeyboardButton(name, url=f"https://t.me/{usr.username}")],
@@ -137,13 +137,13 @@ async def yas(client, message):
     photo = await app.download_media(usr.photo.big_file_id)
     await message.reply_photo(
         photo,
-        caption=f"**[⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝘼𝙇𝙄𝙉𝘼 - 🧑🏻‍💻🖤 خاوەنی بۆت](t.me/MGIMT)\nزانیاری خاوەنی بۆت\n↜︙𝐍𝐀𝐌𝐄 ↬:{name}\n↜︙𝐔𝐒𝐄𝐑 ↬ :@{usr.username}\n↜︙𝐈𝐃 ↬ :`{usr.id}`\n↜︙𝐁𝐈𝐎 ↬: {usr.bio}**",
+        caption=f"**[⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝙃𝙖𝙬𝙖𝙡 - 🧑🏻‍💻🖤 خاوەنی بۆت](t.me/Haawall)\nزانیاری خاوەنی بۆت\n↜︙𝐍𝐀𝐌𝐄 ↬:{name}\n↜︙𝐔𝐒𝐄𝐑 ↬ :@{usr.username}\n↜︙𝐈𝐃 ↬ :`{usr.id}`\n↜︙𝐁𝐈𝐎 ↬: {usr.bio}**",
         reply_markup=InlineKeyboardMarkup(
             [
                 [InlineKeyboardButton(name, url=f"https://t.me/{usr.username}")],
                 [
                     InlineKeyboardButton(
-                        "کەناڵی گەشەپێدەر", url=f"https://t.me/EHS4SS"
+                        "کەناڵی گەشەپێدەر", url=f"https://t.me/Haawall"
                     ),
                 ],
             ]
@@ -151,34 +151,19 @@ async def yas(client, message):
     )
 
 
-@app.on_message(command(["کەناڵ", "کەنال"]))
-async def yas(client, message):
-    usr = await client.get_chat(-1001665233883)
-    name = usr.first_name
-    photo = await app.download_media(usr.photo.big_file_id)
-    await message.reply_photo(
-        photo,
-        caption=f"**[⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝙄𝙌 - کەناڵی سەرچاوە 🧑🏻‍💻](t.me/MGIMT)**\n**جۆینی کەناڵی بۆت بکە بۆ بینینی بابەتی جیاوازتر♥**\n\n** بەستەری کەناڵ :\nhttps://t.me/{usr.username}**",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [InlineKeyboardButton(name, url=f"https://t.me/{usr.username}")],
-            ]
-        ),
-    )
-
 
 @app.on_message(command(["زیرەکی دەستکرد"]))
 async def huhh(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/7713aee1676f475d4ed21.jpg",
-        caption=f"""**[⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝘼𝙇𝙄𝙉𝘼 - زیرەکی دەستکرد🧑🏻‍💻🖤](t.me/MGIMT)**\n\n**بەخێربێی ئەزیزم {message.from_user.mention} بۆ بەشی زیرەکی دەستکرد تایبەت بە سەرچاوەی زیرەك**\n** بۆ بەکارهێنانی بنووسە : iq + پرسیارەکەت ♥⚡**""",
+        caption=f"""**[⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝙃𝙖𝙬𝙖𝙡 - زیرەکی دەستکرد🧑🏻‍💻🖤](t.me/Haawall)**\n\n**بەخێربێی ئەزیزم {message.from_user.mention} بۆ بەشی زیرەکی دەستکرد تایبەت بە سەرچاوەی زیرەك**\n** بۆ بەکارهێنانی بنووسە : iq + پرسیارەکەت ♥⚡**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("﮼محمد˹َّّ", url=f"https://t.me/IQ7amo"),
+                    InlineKeyboardButton("H𝐀𝐖𝐀𝐋 ʲ ↱ˡⁱᶠᵉ↰🐍", user_id=OWNER_ID),
                 ],
                 [
-                    InlineKeyboardButton("⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝘼𝙇𝙄𝙉𝘼", url=f"https://t.me/MGIMT"),
+                    InlineKeyboardButton("⧉• 𝙎𝙊𝙐𝙍𝘾𝞝 𝙃𝙖𝙬𝙖𝙡", url=f"https://t.me/Haawall"),
                 ],
             ]
         ),
@@ -254,10 +239,10 @@ async def vgdg(client: Client, message: Message):
 async def vgdg(client, message):
     await message.reply_text(
         f"""**✧ 𝖶𝖾𝗅𝖼𝗈𝗆𝖾 𝖡𝖺𝖻𝗒,
-𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋 -› [𝑴𝒖𝒉𝒂𝒎𝒎𝒆𝒅 ♪](t.me/IQ7amo)
-𝖢𝗁𝖺𝗇𝗇𝖾𝗅 -› [𝑺𝒐𝒖𝒓𝒄𝒆 𝑨𝒍𝒊𝒏𝒂](t.me/MGIMT)**""",
+𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋 -› [𝑯𝒂𝒘𝒂𝒍 ♪](t.me/Hawaallll)
+𝖢𝗁𝖺𝗇𝗇𝖾𝗅 -› [𝑺𝒐𝒖𝒓𝒄𝒆 𝑯𝒂𝒘𝒂𝒍](t.me/Haawall)**""",
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("نوێکارییەکانی ئەلینا 🍻", url=f"t.me/MGIMT")]]
+            [[InlineKeyboardButton("نوێکارییەکانی هەواڵ 🍻", url=f"t.me/Haawall")]]
         ),
         disable_web_page_preview=True,
     )
@@ -313,7 +298,7 @@ async def delet(client: Client, message: Message):
                 ],
                 [
                     InlineKeyboardButton(
-                        "نوێکارییەکانی ئەلینا 🍻", url=f"https://t.me/MGIMT"
+                        "نوێکارییەکانی هەواڵ 🍻", url=f"https://t.me/Haawall"
                     ),
                 ],
             ]
