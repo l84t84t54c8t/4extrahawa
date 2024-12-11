0dd2833006018d1482f7faddaef40cca93a02299
l888lmn <deakoking85@gmail.com>
2024-12-12 00:09:36 +0300

    Update joinm.py

--- plugins/joinm.py
+++ plugins/joinm.py
@@ -6,7 +6,7 @@ from pyrogram.errors import (ChatAdminRequired, ChatWriteForbidden,
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 
-@app.on_message(filters.incoming & filters.private, group=-1)
+@app.on_message(filters.incoming & filters.private, group=-2)
 async def must_join_channel(app: Client, msg: Message):
     if not MUST_JOIN:
         return
@@ -14,20 +14,24 @@ async def must_join_channel(app: Client, msg: Message):
         try:
             await app.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
+            # Set a default value for channel_name
+            channel_name = f"@{MUST_JOIN}"
             if MUST_JOIN.isalpha():
                 link = "https://t.me/" + MUST_JOIN
             else:
                 chat_info = await app.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
+                channel_name1 = (
+                    chat_info.title
+                )  # Re-assign channel_name if a title is available
+
             try:
                 await msg.reply(
                     f"**• Sorry . . {msg.from_user.mention}\n• You must first join channel to use me\n• Channel : « @{MUST_JOIN} »\n\n• ببووره . . ئەزیزم {msg.from_user.mention}\n• سەرەتا پێویستە جۆینی کەناڵ بکەیت بۆ بەکارهێنانم\n• کەناڵ : « @{MUST_JOIN} »**",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton(
-                                    "ئێرە دابگرە ✅", url=link
-                                ),
+                                InlineKeyboardButton(channel_name1, url=link),
                             ]
                         ]
                     ),
