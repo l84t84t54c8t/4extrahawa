be07c1a804b14421179b04f71afcba47c0ddf4d4
l888lmn <deakoking85@gmail.com>
2025-01-18 20:36:33 +0300

    Update videotxt.py

--- plugins/videotxt.py
+++ plugins/videotxt.py
@@ -6,6 +6,7 @@ from pydub import AudioSegment
 from pyrogram import filters
 from pyrogram.types import Message
 
+
 # --------------------------------------
 
 
@@ -25,9 +26,9 @@ def convert_video_to_text(video_path):
 
 
 @app.on_message(filters.command("vtxt") & filters.reply)
-def convert_video_to_text_cmd(_, message: Message):
+async def convert_video_to_text_cmd(_, message: Message):
     # -------------------------------
-    video_path = message.reply_to_message.download("video.mp4")
+    video_path = await message.reply_to_message.download("video.mp4")
 
     # ------------------------------
     text_result = convert_video_to_text(video_path)
@@ -36,46 +37,48 @@ def convert_video_to_text_cmd(_, message: Message):
     with open("file.txt", "w", encoding="utf-8") as file:
         file.write(text_result)
     # ---------------------------
-    message.reply_document("file.txt")
+    await message.reply_document("file.txt")
 
-    # -------------------------------------
+    # Clean up temporary files
+    os.remove("file.txt")
+    os.remove(video_path)
 
 
 @app.on_message(filters.command(["لادانی", "/remove"], "") & filters.reply)
-def remove_media(client, message: Message):
+async def remove_media(client, message: Message):
     # Fetching the replied message
     replied_message = message.reply_to_message
 
     if replied_message.video:
         # If the replied message is a video, remove either the audio or the
-        # video depending on the command
+        # video
         if len(message.command) > 1:
             command = message.command[1].lower()
-            if command == "video" or command == "ڤیدیۆ" or command == "ڤیدیو":
+            if command in ["video", "ڤیدیۆ", "ڤیدیو"]:
                 # Remove audio
-                file_path = app.download_media(replied_message.video)
+                file_path = await app.download_media(replied_message.video)
                 audio = AudioSegment.from_file(file_path)
                 audio = audio.set_channels(1)
                 audio.export("output.mp3", format="mp3")
-                app.send_audio(message.chat.id, "output.mp3")
+                await app.send_audio(message.chat.id, "output.mp3")
                 os.remove(file_path)
                 os.remove("output.mp3")
-            elif command == "audio" or command == "گۆرانی" or command == "دەنگ":
+            elif command in ["audio", "گۆرانی", "دەنگ"]:
                 # Remove video
-                file_path = app.download_media(replied_message.video)
+                file_path = await app.download_media(replied_message.video)
                 os.system(f"ffmpeg -i {file_path} -c copy -an output.mp4")
-                app.send_video(message.chat.id, "output.mp4")
+                await app.send_video(message.chat.id, "output.mp4")
                 os.remove(file_path)
                 os.remove("output.mp4")
             else:
-                app.send_message(
+                await app.send_message(
                     message.chat.id,
                     "**فەرمانت هەڵە بەکارهێنا بەم شێوازەیە :\nلادانی گۆرانی یان لادانی ڤیدیۆ\n /remove audio یان /remove video**",
                 )
         else:
-            app.send_message(
+            await app.send_message(
                 message.chat.id,
                 "**تکایە دیاری بکە کە ئایا دەنگ یان ڤیدیۆ بە بەکارهێنانی /remove audio  /remove video**",
             )
     else:
-        app.send_message(message.chat.id, "**ڕیپلەی ڤیدیۆت نەکردووە**")
+        await app.send_message(message.chat.id, "**ڕیپلەی ڤیدیۆت نەکردووە**")
