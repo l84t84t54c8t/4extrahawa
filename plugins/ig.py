91558565aebf1eb33204603a30c8d6ba693e4f8c
l888lmn <deakoking85@gmail.com>
2025-01-28 14:32:33 +0300

    Update ig.py

--- plugins/ig.py
+++ plugins/ig.py
@@ -9,24 +9,29 @@ instagram_url_pattern = r"(https?://(?:www\.)?instagram\.com/[-a-zA-Z0-9@:%._\+~
 @app.on_message(filters.regex(instagram_url_pattern))
 async def down(app, message):
     try:
-        link = message.text
+        link = message.text.strip()
         json_data = {"url": link}
+        
+        # Send POST request to download Instagram video
         response = requests.post(
             "https://insta.savetube.me/downloadPostVideo", json=json_data
         ).json()
 
-        # Extract video and thumbnail
-        thu = response["post_video_thumbnail"]
-        video = response["post_video_url"]
+        # Extract video and thumbnail details
+        thu = response.get("post_video_thumbnail")
+        video = response.get("post_video_url")
+
+        if not thu or not video:
+            raise ValueError("Invalid response data")
 
         # Send thumbnail as a photo
         await message.reply_photo(
             thu,
-            caption="**← کەمێک چاوەڕێ بکە .. ڤیدیۆ دادەبەزێت ...\n⧉• لەلایەن : @IQMCBOT**",
+            caption="**← کەمێک چاوەڕێ بکە .. ڤیدیۆ دادەبەزێت ...\n⧉• لەلایەن : @{app.me.username}**",
         )
 
         # Send video directly
-        caption = "**✅꒐ بە سەرکەوتوویی داگرترا\n🎸꒐ بۆتی @{app.username}**"
+        caption = f"**✅꒐ بە سەرکەوتوویی داگرترا\n🎸꒐ بۆتی @{app.me.username}**"
         await app.send_video(message.chat.id, video, caption=caption)
 
     except Exception as e:
