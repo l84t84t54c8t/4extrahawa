77df2d774429c0f44443c93955b6d3de9d55c95d
l888lmn <deakoking85@gmail.com>
2025-05-22 08:50:03 +0300

    Update pretender.py

--- plugins/pretender.py
+++ plugins/pretender.py
@@ -2,7 +2,7 @@ from typing import Dict, Union
 
 from AlinaMusic import app
 from config import MONGO_DB_URI
-from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+from pymongo import AsyncMongoClient as MongoCli
 from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.types import Message
