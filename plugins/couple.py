037b71d616293ba77252a538019279db58da4240
l888lmn <deakoking85@gmail.com>
2025-07-14 19:36:12 +0300

    Update couple.py

--- plugins/couple.py
+++ plugins/couple.py
@@ -147,7 +147,7 @@ async def couples(app, message):
 """
         await app.send_chat_action(message.chat.id, ChatAction.UPLOAD_PHOTO)
         await message.reply_photo(
-            f"downloads/test_{cid}.png",
+            f"test_{cid}.png",
             caption=TXT,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton(text="👻 خاوەنی بۆت 👻", user_id=OWNER)]]
@@ -163,6 +163,6 @@ async def couples(app, message):
         try:
             os.remove("pfp.png")
             os.remove("pfp1.png")
-            os.remove(f"downloads/test_{cid}.png")
+            os.remove(f"test_{cid}.png")
         except Exception:
             pass
